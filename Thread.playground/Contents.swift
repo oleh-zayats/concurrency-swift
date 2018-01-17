a6078e855a35743af0c2d4c17900d1eec97b3298
Oleh Zayats <ozayats@gmail.com>
2018-01-17 23:52:47 +0200

    Added thread playground

---
+++ Thread.playground/Contents.swift
@@ -0,0 +1,50 @@
+
+import Foundation
+import PlaygroundSupport
+
+PlaygroundPage.current.needsIndefiniteExecution = true
+
+/*******/
+
+class Parameters: NSObject {
+    let parameter1: String
+    let parameter2: String
+    
+    init(parameter1: String, parameter2: String) {
+        self.parameter1 = parameter1
+        self.parameter2 = parameter2
+    }
+}
+
+class Handler: NSObject {
+    @objc func execute(with parameters: Parameters) {
+        print("0: executed with params: \(parameters.parameter1) & \(parameters.parameter2)\n")
+    }
+}
+
+let handler = Handler()
+let parameters = Parameters(parameter1: "001", parameter2: "002")
+
+let threadOne = Thread(target: handler, selector: #selector(Handler.execute(with:)), object: parameters)
+threadOne.start()
+
+/*******/
+
+let sleepInterval: TimeInterval = 3
+
+final class CustomThread: Thread {
+    override func main() {
+        print("1: start thread, sleep for \(sleepInterval) seconds")
+        Thread.sleep(forTimeInterval: sleepInterval)
+        print("2: wake up, exit thread\n")
+    }
+}
+
+let threadTwo = CustomThread()
+threadTwo.stackSize = 1024 * 16
+threadTwo.start()
+
+
+
+
+
